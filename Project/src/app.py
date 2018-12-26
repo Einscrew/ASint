f834670bf8fdc0f04cec54a109c712aee55f01a3
Einscrew <manel.rodrigo.reis@gmail.com>
2018-12-26 19:30:45 +0000

    shit

--- Project/src/app.py
+++ Project/src/app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-from flask import Flask, Response, abort, render_template, request, jsonify, redirect
+from flask import Flask, Response, abort, make_response, render_template, request, jsonify, redirect, url_for
 from itertools import chain
 import db
+
+from werkzeug.contrib.cache import SimpleCache
 
 from functools import wraps
 
@@ -9,28 +11,64 @@ from random import randint
 import requests
 import json
 
+import datetime
+
 app = Flask(__name__)
 app.config.from_pyfile('settings')
+
+cache = SimpleCache()
 
 db = db.Db()
 
 with open("../keys.json",'r') as f:
-	FENIX_API = json.load(f)
-	
-FENIX_API['redirectURI']= 'http://127.0.0.1:5000/messages'
+	APP = json.load(f)
 
+
+with open("../secret", 'rb') as f:
+	app.secret_key = f.read()
+
+
+APP['redirectURI'] = 'http://127.0.0.1:5000/'
+APP['loginURI'] = 'https://fenix.tecnico.ulisboa.pt/oauth/userdialog?client_id='+str(APP['clientID'])+'&redirect_uri='+APP['redirectURI']
+
+@app.after_request
+def add_header(response):
+	"""
+	Add headers to both force latest IE rendering engine or Chrome Frame,
+	and also to cache the rendered page for 10 minutes.
+	"""
+	response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+	response.headers['Cache-Control'] = 'public, max-age=0'
+	return response
 
 def validAdmin(username, password):
-    return username == app.config['USER'] and password == app.config['PASS']
+	return username == app.config['USER'] and password == app.config['PASS']
 
 def admin(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        auth = request.authorization
-        if not auth.username or not auth.password or not validAdmin(auth.username, auth.password):
-            return Response('Credentials required!', 401, {'WWW-Authenticate': 'Basic realm="Login!"'})
-        return f(*args, **kwargs)
-    return wrapper
+	@wraps(f)
+	def wrapper(*args, **kwargs):
+		auth = request.authorization
+		if not auth.username or not auth.password or not validAdmin(auth.username, auth.password):
+			return Response('Credentials required!', 401, {'WWW-Authenticate': 'Basic realm="Login!"'})
+		return f(*args, **kwargs)
+	return wrapper
+
+def login_required(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		cookie = request.cookies.get('access_token')
+		print('COOKIE IN REQUEST?:', cookie)
+		if cookie == None or cookie != cache.get('access_token'):#or 'username' not in session:#c is None:
+			cache.clear()
+			request.get('https://id.tecnico.ulisboa.pt/cas/logout')
+			return redirect(APP['loginURI'])
+		if cache.get('username') not in kwargs.values(): #and cookie != session['username']:
+			cache.clear()
+			return abort(401)#redirect('/login',302)#Response('Credentials required!', 302, {'WWW-Authenticate': 'Basic realm="Login!"','Location':'http://127.0.0.1:5000/'})
+			#return Response(make_response('/login'), 302)#APP['loginURI'])
+
+		return f(*args, **kwargs)
+	return decorated_function
 
 '''ADMIN ENDPOINTS'''
 #Manage Buildings
@@ -48,6 +86,7 @@ def buildingsManagement():
 				buildingList.add({"_id": ID, "name": name, "location": { "lat": lat, "lon": lon }})
 	db.insertBuildings(buildingList)
 	return "done"
+
 
 @app.route('/API/admin/buildings',methods=['POST'])
 @admin
@@ -174,7 +213,9 @@ def usersInRange(istID):
 
 #List users in range
 @app.route('/API/users/<string:istID>/message/received', methods=['POST'])
+@login_required
 def received(istID):
+	print('COOKIE IN REQUEST?:', request.cookies.get('access_token'))
 	return jsonify(db.getUserMessages(istID))
 
 #Updates user's building
@@ -195,11 +236,22 @@ def updateBuilding(istID):
 
 @app.route('/')
 def hello_world():
-	istID = 'ist' + str(randint(150000, 200000))
-	if db.insertUser(istID, {'lat':12,'lon':241}, 10):
-		return render_template("webApp.html", istID=istID)
-	else:
-		abort(jsonify(message="user already registered???"))
+	code = request.args.get('code')
+	print(cache)
+	#	input('fdssss	')
+
+	if code is None and cache.get('access_token') is None:
+		return redirect(APP['loginURI'])
+
+	elif code is not None and cache.get('access_token') is None:
+		cache.add('username',getUserInfo(code),timeout = 5)
+		return redirect('/')
+	
+	db.insertUser(cache.get('username'), {'lat':12,'lon':241}, 10)
+	resp = make_response(render_template("webApp.html", istID=cache.get('username')))
+
+	resp.set_cookie('access_token', cache.get('access_token'))#, expires=(datetime.datetime.now()+datetime.timedelta(seconds=30)))	
+	return resp
 
 @app.route('/logout')
 def logout(istID):
@@ -207,27 +259,25 @@ def logout(istID):
 	#Here redirect to login page again
 	#return render_template("webApp.html", istID)
 
+@app.route('/login')
+def log():
+	return render_template("mainPage.html")
 
-@app.route('/messages')
-def messages():
 
-	# get code provided by fenixedu
-	code = request.args.get('code')
-	access_token_request_url = 'https://fenix.tecnico.ulisboa.pt/oauth/access_token'
-	request_data = {'client_id': int(FENIX_API['clientID']), 'client_secret': FENIX_API['clientSecret'],
-			'redirect_uri': FENIX_API['redirectURI'], 'code': code, 'grant_type': 'authorization_code'}
+def getUserInfo(code):
+    access_token_request_url = 'https://fenix.tecnico.ulisboa.pt/oauth/access_token'
+    request_data = {'client_id': int(APP['clientID']), 'client_secret': APP['clientSecret'],
+            'redirect_uri': APP['redirectURI'], 'code': code, 'grant_type': 'authorization_code'}
 
-	request_access_token = requests.post(access_token_request_url, data=request_data)
+    reqAccessToken = requests.post(access_token_request_url, data=request_data)
 
-	access_token = request_access_token.json()['access_token']
-	refresh_token = request_access_token.json()['refresh_token']
-	token_expires =  request_access_token.json()['expires_in']
+    token = reqAccessToken.json().get('access_token')
+    cache.add('access_token', token, timeout=5)
+    print('access token in cache',cache.get('access_token'))
 
-	params = {'access_token': access_token}
-	request_info = requests.get('https://fenix.tecnico.ulisboa.pt/api/fenix/v1/person', params=params)
 
-	print(request_info.json())
-	return "<p>yeyy</p>"#render_template("messages.html")
+    request_info = requests.get('https://fenix.tecnico.ulisboa.pt/api/fenix/v1/person', params={'access_token': token})
+    return request_info.json().get('username')
 
 if __name__ == '__main__':
 	app.run()
