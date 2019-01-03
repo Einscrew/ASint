80f4052563df3a88170ff0393abf1e0bb97f5ba3
catarinaacfa <catariinaacfa@gmail.com>
2019-01-03 19:10:17 +0000

    files added

--- Project/src/main.py
+++ Project/src/main.py
@@ -20,11 +20,11 @@ cache = SimpleCache()
 
 db = db.Db()
 
-with open("../keys.json",'r') as f:
+with open("keys.json",'r') as f:
 	APP = json.load(f)
 
 
-with open("../secret", 'rb') as f:
+with open("secret", 'rb') as f:
 	app.secret_key = f.read()
 
 
