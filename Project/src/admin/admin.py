0f5d6a74dcd87a6939f9ac9410247caa36f99f1e
Einscrew <manel.rodrigo.reis@gmail.com>
2019-01-05 20:11:37 +0000

    logging to be beautified

--- Project/src/admin/admin.py
+++ Project/src/admin/admin.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import requests
 import json
-URL ='http://127.0.0.1:5000'
+#URL = 'https://asint-2018.appspot.com'
+URL = 'http://127.0.0.1:5000'
 
 
 def SetBuildings(file='buildings.json'):
@@ -12,32 +13,120 @@ def SetBuildings(file='buildings.json'):
 		return 'error'
 	return requests.put(URL+'/API/admin/buildings/manage', json=build, auth=('user', 'pass'))
 
-
-def LoggedUsers(filters=None):
-	if filters is None:
+def LoggedUsers():
+	i = input('Logged users:\n\t1 - all\n\t2 - by building\n\t\n>> ')
+	if i == '1':
 		endpoint='/API/admin/users/loggedin'
 		r = requests.post(URL+endpoint, auth=('user', 'pass'))
-		return r.content if r.status_code == 200 else None
-	else:
+		if r.status_code == 200:
+			print('received:',r.json())
+			print(">"*20)
+			print(*r.json(), sep='\n')
+			print("<"*20)
+		else:
+			print('[{}] Error'.format(r.status_code))
+	elif i == '2':
+		b = getBuilding()
 		s = set()
-		for f in filters:
-			endpoint='/API/admin/buildings/'+f+'/users'
+		for f in b:
+			endpoint='/API/admin/buildings/'+f['_id']+'/users'
 			r = requests.post(URL+endpoint, auth=('user', 'pass'))
 			if r.status_code == 200:
 				s = s.union(set(r.json().get('users')))
-		return s
+		print('received:',s)
+		print(">"*20)
+		print(*s, sep='\n')
+		print("<"*20)
+	else:
+		print('Not a valid option')
+		return
 
-def Logs(filters= None):
-	if not filters or "user" not in filters and "building" not in filters:
-		endpoint='/API/admin/logs'
-	elif "user" in filters:
-		endpoint='/API/admin/users/'+str(filters["user"])+'/logs'	
-	elif "building" in filters:
-		endpoint='/API/admin/buildings/'+str(filters["building"])+'/logs'
-	return requests.post(URL+endpoint, auth=('user', 'pass'))
+def getUsers():
+	d = dict()
+	resp=requests.post(URL+'/API/admin/users', auth=('user', 'pass'))
+	if resp.status_code == 200 and 'application/json' in resp.headers['Content-Type']:
+		l = resp.json()
+		print('Users:')
+		for num , v in enumerate(l,1):
+			print('\t{:3} {}'.format(num, v))
+		num = input('\nType the number of the user to see their active users [enter to skip]')
+		if len(num) ==0:
+			return None
+		try:
+			return [l[int(num)-1]]
+		except:
+			return None
+	else:
+		print('[{}] No users available'.format(resp.status_code))
+		return None
 
+def getBuilding():
+	resp = requests.post(URL+'/API/admin/buildings', auth=('user','pass'))
+	if resp.status_code == 200 and 'application/json' in resp.headers['Content-Type']:
+		buildings = resp.json()
+		d = dict()
+		print('Buildings:')
+		for num, b in enumerate(buildings,1):
+			d[num] = {'_id':b['_id'],'name':b['name'], 'num':num}
+			print('\t{:3} {}'.format(num, b['name']))
+		num = input('\nType the number(s) of the building(s) to see their active users: ')
+		if len(num) == 0:
+			return None
+		try:
+			return [d[int(n)] for n in num.split(',') ]
+		except:
+			return None
+	else:
+		print('[{}] No buildings available'.format(resp.status_code))
+		return None
+
+def logEntry(e):
+	#\xF0\x9F\x93\xA8
+	#\xF0\x9F\x93\xA9
+	t = e.get('type')
+	if t in ('sent','received'):
+		return '{}][{}'.format('msg'.center(len('movement')), ' user '+e.get('info')[0])
+		#if e.get('info').get()
+	if t == 'movement':
+		return 'movement'
+
+
+
+def format(listlogs):
+	return [ "[{}][{}]".format(l[0].split(',')[1][1:-3] , logEntry(l[1])) for l in listlogs]
+
+
+def Logs():
+	i = input('Logs by:\n\t1 - user\n\t2 - building\n\tother - both\n>> ')
+	u = None
+	b = None
+	try:
+		if i != '2':
+			u = getUsers()[0]
+		if i != '1':
+			b = getBuilding()[0]['_id']
+	except:
+		print("Problems on input")
+		return
+
+	if u and b:
+		endpoint = '/API/admin/buildings/{}/user/{}/logs'.format(b,u)
+	elif u:
+		endpoint='/API/admin/users/{}/logs'.format(u)
+	elif b:
+		endpoint='/API/admin/buildings/{}/logs'.format(b)
+
+	resp = requests.post(URL+endpoint, auth=('user', 'pass'))
+	if resp.status_code == 200:
+		print('received:',resp.json())
+		print(">"*20)
+		print(*format(resp.json()), sep='\n')
+		print("<"*20)
+	else:
+		print('[{}] Error'.format(resp.status_code))
 
 def createBot():
+	print('Create bot')
 	resp = requests.post(URL+'/API/admin/buildings', auth=('user','pass'))
 	if resp.status_code == 200 and 'application/json' in resp.headers['Content-Type']:
 		buildings = resp.json()
@@ -61,50 +150,6 @@ def createBot():
 	else:
 		print("Error occour")
 
-def getUsers():
-	resp=requests.post(URL+'/API/admin/users', auth=('user', 'pass'))
-	if resp.status_code == 200 and 'application/json' in resp.headers['Content-Type']:
-		l = resp.json()
-		for num , v in enumerate(l):
-			print('{:3} {}'.format(num, b['name']))
-
-def getBuilding():
-	resp = requests.post(URL+'/API/admin/buildings', auth=('user','pass'))
-	if resp.status_code == 200 and 'application/json' in resp.headers['Content-Type']:
-		buildings = resp.json()
-		d = dict()
-		num = 1
-		for b in buildings:
-			d[num] = b['_id']
-			print('{:3} {}'.format(num, b['name']))
-			num = num+1
-
-		num = input('\nType the number(s) of the building(s) to see their active users: ')
-		
-		r = [d[int(n)] for n in num.split(',')]
-		print(r)
-		return r
-	return []
-
-
-def selectBuilding():
-	resp = requests.post(URL+'/API/admin/buildings', auth=('user','pass'))
-	if resp.status_code == 200 and 'application/json' in resp.headers['Content-Type']:
-		buildings = resp.json()
-		d = dict()
-		num = 1
-		for b in buildings:
-			d[num] = b['_id']
-			print('{:3} {}'.format(num, b['name']))
-			num = num+1
-
-		num = input('\nType the number(s) of the building(s) to see their active users: ')
-		
-		r = [d[int(n)] for n in num.split(',')]
-		print(r)
-		return r
-
-
 
 if __name__ == '__main__':
 	while(True):
@@ -122,17 +167,10 @@ if __name__ == '__main__':
 				else:
 					SetBuildings()
 			elif i == 2:
-				i = input('Logged users by:\n\t1 - user\n\t2 - building\n\tother - both\n>> ')
-				print(LoggedUsers(filters=getBuilding()))
+				LoggedUsers()
 			elif i == 3:
-				i = input('Logs by:\n\t1 - user\n\t2 - building\n\tother - both\n>> ')
-				f = None
-				if i == "user" or i == "building":
-					f = dict()
-					f[i] = input(i+ "number: ")
-				print(Logs(filters = f).json())
+				Logs()
 			elif i == 4:
-				i = print('Create bot')
 				createBot()
 
 		except TypeError:
